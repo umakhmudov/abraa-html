3d91c8d70d0ab5c25b39afbdf6d77b9b5b5f29b7
Ulugbek Makhmudov <ulugbek_u@hotmail.com>
2018-10-02 23:21:28 +0400

    mobile search button and toggle in the navigation

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 $(document).ready(function(){
     $('#headerSearchType').selectBox();
     $('#headerLang').selectBox();
+
+    $('.mobile_search_toggle_btn').click(function(e){
+        e.preventDefault();
+        $('.header-search-section').toggleClass('active');
+    })
 })
