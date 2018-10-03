6c6343f04c8996372bd6631315a96b631159f441
Ulugbek Makhmudov <ulugbek_u@hotmail.com>
2018-10-03 05:22:45 +0400

    home page completed with sample images and icons

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+
+    //init selectbox plugin
     $('#headerSearchType').selectBox({
         mobile: true
     });
@@ -6,8 +8,12 @@ $(document).ready(function(){
         mobile: true
     });
 
+    //toggle search bar on mobile
     $('.mobile_search_toggle_btn').click(function(e){
         e.preventDefault();
         $('.header-search-section').toggleClass('active');
     })
+
+    //init the slider plugin
+    $('#categories_slider').multislider();
 })
