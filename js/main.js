ffac869535416dc78e062568271e3d26fb768811
Ulugbek Makhmudov <ulugbek_u@hotmail.com>
2018-10-06 18:12:08 +0400

    category page

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-    //init selectbox plugin
+    //init selectbox plugins
 
     //for dropdown on search bar
     $('#headerSearchType').selectBox({
@@ -21,6 +21,27 @@ $(document).ready(function(){
     $('.mobile_search_toggle_btn').click(function(e){
         e.preventDefault();
         $('.header-search-section').toggleClass('active');
+    });
+
+    //match heights on product boxes
+    $('.product-box').matchHeight();
+
+    //collapse/expand product filters
+    $('.collapse-filter-btn').click(function(e){
+        e.preventDefault();
+        var collapse_area_id = $(this).attr('data-collapse-section');
+        var collapse_area = $('#'+collapse_area_id);
+        $(this).toggleClass('collapsed');
+
+        if($(this).hasClass('collapsed')){
+            //if it's collapsed, set plus sign and hide the area
+            collapse_area.addClass('hidden');
+            $(this).find('.glyphicon').removeClass('glyphicon-minus').addClass('glyphicon-plus');
+        } else {
+            //if it's expanded, set minus sign and show the area
+            collapse_area.removeClass('hidden');
+            $(this).find('.glyphicon').removeClass('glyphicon-plus').addClass('glyphicon-minus');
+        }
     })
 
     //init the slider plugin
